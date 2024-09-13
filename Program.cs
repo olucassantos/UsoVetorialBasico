7388a8236459c6044c62050ca68b47a7588414c4
Lucas Santos <lucas.santos@sp.senai.br>
2024-09-12 21:41:59 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoVetorialBasico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidade = 5;
+            string[] nomes = new string[quantidade];
+
+            // Cria um laço para solicitar nomes
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.Write("Escreva um nome: ");
+                nomes[i] = Console.ReadLine();
+            }
+
+            // Percorrer o vetor e escrever seus valores
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine(nomes[i]);
+            }
+
+            // Percorrer o vetor e escrever seus valores
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
